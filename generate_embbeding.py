cf35d902ebfab72e9b6c1ac4f0de38ef07c6beb0
OmarKhalifa <superomarkhalifa@gmail.com>
2024-12-01 23:31:17 +0200

    Update generate_embbeding.py

--- generate_embbeding.py
+++ generate_embbeding.py
@@ -1,4 +1,4 @@
-!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m1.txt" --epoch 1 --dimensions 32
-!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m2.txt" --epoch 1 --dimensions 32
-!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m3.txt" --epoch 1 --dimensions 32
-!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m4.txt" --epoch 1 --dimensions 32
+!python "/kaggle/working/GNN_DDI/GNN/main.py" --input "/DDI/data5" --features "/kaggle/working/GNN_DDI/DDI/data5/featuers_m1.txt" --epoch 1 --dimensions 32
+!python "/kaggle/working/GNN_DDI/GNN/main.py" --input "/DDI/data5" --features "/kaggle/working/GNN_DDI/DDI/data5/featuers_m2.txt" --epoch 1 --dimensions 32
+!python "/kaggle/working/GNN_DDI/GNN/main.py" --input "/DDI/data5" --features "/kaggle/working/GNN_DDI/DDI/data5/featuers_m3.txt" --epoch 1 --dimensions 32
+!python "/kaggle/working/GNN_DDI/GNN/main.py" --input "/DDI/data5" --features "/kaggle/working/GNN_DDI/DDI/data5/featuers_m4.txt" --epoch 1 --dimensions 32
