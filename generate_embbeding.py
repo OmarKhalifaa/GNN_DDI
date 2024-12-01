e70ad89b471bdbb52951063bec36d38394f621e6
OmarKhalifa <superomarkhalifa@gmail.com>
2024-12-01 23:14:45 +0200

    Update generate_embbeding.py

--- generate_embbeding.py
+++ generate_embbeding.py
@@ -1,4 +1,4 @@
-!python "/kaggle/working/GNN_DDI/kaggle/working/GNN_DDI/GNNmain.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m1.txt" --epoch 1 --dimensions 32
+!python "/kaggle/working/GNN_DDI/GNNmain.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m1.txt" --epoch 1 --dimensions 32
 !python "/kaggle/working/GNN_DDI/GNNmain.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m2.txt" --epoch 1 --dimensions 32
 !python "/kaggle/working/GNN_DDI/GNNmain.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m3.txt" --epoch 1 --dimensions 32
 !python "/kaggle/working/GNN_DDI/GNNmain.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m4.txt" --epoch 1 --dimensions 32
