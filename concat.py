d27c85f414d55dda12031200fc6fffc1edd09f0f
OmarKhalifa <superomarkhalifa@gmail.com>
2024-12-02 23:17:48 +0200

    Update concat.py

--- concat.py
+++ concat.py
@@ -2,12 +2,10 @@ from pandas import DataFrame
 import numpy as np
 import pandas as pd
 import csv
-
-x1=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelssd1_d_32.csv", header=None , sep=',')).tolist())
-x2=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelssd2_d_32.csv", header=None , sep=',')).tolist())
-x3=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelssd3_d_32.csv", header=None , sep=',')).tolist())
-x4=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelssd4_d_32.csv", header=None , sep=',')).tolist())
-
+x1=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelss1_d_32.csv", header=None , sep=',')).tolist())
+x2=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelss2.csv", header=None , sep=',')).tolist())
+x3=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelss3.csv", header=None , sep=',')).tolist())
+x4=np.array(np.array(pd.read_csv("/kaggle/working/GNN_DDI/DDI/data5/final_modelss4.csv", header=None , sep=',')).tolist())
 def chang_to_array(s):
   # print(s.shape)
   final_model = []
